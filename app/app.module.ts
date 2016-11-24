862dfa464f3c9e52a2551678ac1e082a3e0d2e11
swfz <sawafuji.09@gmail.com>
2016-11-24 09:27:12 +0900

    7.http implement update,create,delete

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { RouterModule }  from '@angular/router';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }        from './app.component';
 import { DashboardComponent }  from './dashboard.component';
@@ -11,11 +12,16 @@ import { HeroService }         from './hero.service';
 
 import { AppRoutingModule }    from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
